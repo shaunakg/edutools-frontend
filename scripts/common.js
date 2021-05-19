536dba0e3889615789a2044e4a4a0a33cfd2b6d3
Shaunak G <hello@srg.codes>
2021-05-19 14:55:33 +1000

    redirect notification

---
+++ scripts/common.js
@@ -0,0 +1,13 @@
+if (new URLSearchParams(location.search).get("from_old")) {
+
+    let siteHasMovedWarning = document.createElement("div");
+    siteHasMovedWarning.innerHTML = "Welcome! We have rebranded to Edutools for Notion, a full suite of education tools for Notion. Everything from before has stayed the same.<br><span class=small>(click to close)</span>";
+    siteHasMovedWarning.classList.add("site-has-moved");
+
+    siteHasMovedWarning.onclick = e => {
+        e.target.style.display = "none";
+    }
+
+    document.querySelector("article").prepend(siteHasMovedWarning);
+
+}
